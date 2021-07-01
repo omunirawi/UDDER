e861aa2ea222422370d744e5f51d544adb6e5d27
Obay M. Y. Almunirawi <omunirawi@Obays-MacBook.local>
2021-07-01 23:59:02 +0300

    create UDDER project

---
+++ UDDER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDDER
+//
+//  Created by Obay M. Y. Almunirawi on 01/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
